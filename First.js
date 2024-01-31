59c8b0c61279d822d7c9e62284259f3ab074f278
Muhammad Uzair <153905110+MrUzair0@users.noreply.github.com>
2024-01-31 14:23:40 +0500

    Assignment JS

---
+++ First.js
@@ -0,0 +1,50 @@
+Q1 : 
+alert ("Welcome to MrUzair's website");
+Q2 :
+alert ("Error ! Please Enter a valid Password" );
+Q3 :
+alert ("Hint : Use line break");
+Q4 :
+alert ("Firstly");
+alert ("Secondly")
+alert("Thirdly")
+Q7 :
+var Username;
+Q8 :
+var myName;
+alert (myName ="Uzair")
+Q9 :
+var message="Hello Peoples"
+alert (message)
+Q10 :
+var name = "M.Uzair"
+var age = 19
+var rollno = 56
+alert (name)
+alert (age)
+alert (rollno);
+Q11 :
+var Email = "misteruzair224400@gmail.com"
+alert(Email);
+Q12 :
+console.log(Email);
+Q13 :
+var border = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert (border);
+Q14 :
+var Age
+alert (Age=19);
+Q15 :
+var no = "You have visited this website x times"
+alert (no)
+Q16 :
+var birthYear = 2004
+alert (birthYear)
+Q17 :
+var visitorName = "Muhammad Uzair"
+var product = "shirt"
+var Quantity = 7
+var Store = "www.outfitters.com"
+alert (visitorName + " " +product + " " + Quantity + " " + "from" + " " +Store)
+
+
